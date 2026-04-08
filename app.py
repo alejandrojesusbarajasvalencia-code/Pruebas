40617a74a0d1406dd4004f425f8630c352481ee1
Alejandro Barajas <alejandrojesusbarajasvalencia@gmail.com>
2026-04-07 23:29:01 -0600

    detalles y comentarios

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 nombre = ""
 
-
-def ingresar_nombre(nombre):
+## funciones
+def ingresar_nombre():
     nombre = input("Ingrese un nombre:\n")
     return nombre
 
@@ -15,6 +15,7 @@ def mostrar_nombre(nombre):
 
 def borrar_nombre(nombre):
     nombre = ""
+    print("Se ha borrado el nombre")
     return nombre
 
 
@@ -25,26 +26,33 @@ def actualizar_nombre(nombre):
         nuevo = input("Ingrese el nuevo nombre:\n")
         nombre = nuevo
     return nombre
+##
 
-
+## primera iteracion y menu repetitivo
 opcion = 1
 
 while opcion != 0:
     print(
         "1) Ingresar nombre\n2) Borrar nombre\n3) Actualizar nombre\n4) Mostrar nombre\n0) Salir"
     )
-    opcion = int(input("Ingresa lo que quieres hacer:\n"))
+    try:
+        opcion = int(input("Ingresa lo que quieres hacer:\n"))
+    except ValueError:
+        print("Ingrese una opcion valida")
+        continue
 
+    ## opciones llamadas en main
     if opcion == 1:
-        nombre = ingresar_nombre(nombre)
+        nombre = ingresar_nombre()
 
     elif opcion == 2:
         nombre = borrar_nombre(nombre)
-        if nombre == "":
-            print("Se ha borrado el nombre")
 
     elif opcion == 3:
         nombre = actualizar_nombre(nombre)
 
     elif opcion == 4:
         mostrar_nombre(nombre)
+
+    else:
+        print("Esa opcion no existe, intenta con otra")
